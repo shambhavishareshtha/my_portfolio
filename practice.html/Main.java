56607f89df8ba4a2be6ec660e155d4418bbed15d
shambhavishareshtha <shambhavi.25bcs10183@sst.scaler.com>
2025-10-05 16:59:58 +0530

    Initial commit

---
+++ practice.html/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      productoftwo(2,5);
+    }
+    static void productoftwo(int a,int b){
+      int product=a*b;
+      System.out.print(product);
+      }
+  }
+       
+        
+
+
+
---
+++ practice.html/first.javascript
@@ -0,0 +1,4 @@
+const hi=124
+let bye=34
+ bye=55
+console.log(bye);
